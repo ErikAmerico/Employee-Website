172231628cf64ef0aad605581343d780560b74b5
Erik Olson <erikamerico1991@gmail.com>
2023-09-08 22:38:14 -0400

    With the task of creating header in mind, added simple typeDef and resolver to avoid GraphQL error. removed unneccessary imports from mainJsx and appJsx

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,17 @@
 const { signToken, AuthenticationError } = require("../utils/auth");
 
 const resolvers = {
-    
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({
+                    _id: context.user._id,
+                }).select("-__v -password");
+                return userData;
+            }
+            throw new AuthenticationError("Not logged in");
+        },
+    },
 };
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,14 @@
 const typeDefs = `
-
+    type User {
+        _id: ID
+        username: String
+        email: String
+        password: String
+        role: String
+    }
+    type Query {
+        me: User
+    }
 `;
 
 module.exports = typeDefs;
