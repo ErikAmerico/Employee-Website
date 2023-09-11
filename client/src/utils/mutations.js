1ecd6ccdfcf5ca6cb45e0673c853aa8507489e23
Erik Olson <erikamerico1991@gmail.com>
2023-09-11 18:28:03 -0400

    can add company, and add user when creating company, the user is not associated with company yet though

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,39 +1,69 @@
 import { gql } from "@apollo/client";
 
 export const ADD_COMPANY = gql`
-    mutation AddCompany($name: String!, $type: String!, $logo: String) {
-        addCompany(name: $name, type: $type, logo: $logo) {
-            _id
-            companyId
-            name
-            type
-            logo
-        }
+  mutation AddCompany($name: String!, $type: String!, $logo: String) {
+    addCompany(name: $name, type: $type, logo: $logo) {
+      _id
+      companyId
+      name
+      type
+      logo
     }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation AddUser(
+    $firstName: String!
+    $lastName: String!
+    $role: String!
+    $title: String!
+    $email: String!
+    $phone: String!
+    $password: String!
+    $profileImage: String
+  ) {
+    addUser(
+      firstName: $firstName
+      lastName: $lastName
+      role: $role
+      title: $title
+      email: $email
+      phone: $phone
+      password: $password
+      profileImage: $profileImage
+    ) {
+      token
+      user {
+        email
+        firstName
+        lastName
+      }
+    }
+  }
 `;
 
 export const LOGIN = gql`
-    mutation Login($email: String!, $password: String!) {
-        login(email: $email, password: $password) {
-            token
-            user {
-                _id
-                userId
-                firstName
-                lastName
-                role
-                title
-                company {
-                    _id
-                    companyId
-                    name
-                    type
-                    logo
-                }
-                email
-                phone
-                profileImage
-            }
+  mutation Login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        userId
+        name
+        role
+        title
+        company {
+          _id
+          companyId
+          name
+          type
+          logo
         }
+        email
+        phone
+        profileImage
+      }
     }
+  }
 `;
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,89 +2,90 @@ const { signToken, AuthenticationError } = require("../utils/auth");
 const { User, Company, Post, Comment } = require("../models/Company");
 
 const resolvers = {
-    Query: {
-        me: async (parent, args, context) => {
-            if (context.user) {
-                const userData = await User.findOne({
-                    _id: context.user._id,
-                }).select("-__v -password");
-                return userData;
-            }
-            throw new AuthenticationError("Not logged in");
-        },
-        company: async (parent, { companyId }) => {
-            return await Company.findOne({ companyId })
-                .populate("users")
-                .populate({
-                    path: "users",
-                    populate: "posts",
-                });
-        },
-        //find all users in a company
-        users: async (parent, { companyId }) => {
-            const params = companyId ? { companyId } : {};
-            return await User.find(params).populate("company");
-        },
-        //find all posts in a company
-        posts: async (parent, { companyId }) => {
-            const params = companyId ? { companyId } : {};
-            return await Post.find(params).populate("user").populate({
-                path: "comments",
-                populate: "user",
-            });
-        },
-        //find a single post
-        post: async (parent, { postId }) => {
-            const params = postId ? { postId } : {};
-            return await Post.findOne(params).populate("user").populate({
-                path: "comments",
-                populate: "user",
-            });
-        },
-        //find a selected User
-        user: async (parent, { userId }) => {
-            return await User.findOne({ userId }).populate("company");
-        },
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({
+          _id: context.user._id,
+        }).select("-__v -password");
+        return userData;
+      }
+      throw new AuthenticationError("Not logged in");
     },
-    Mutation: {
-        //add a new company
-        addCompany: async (parent, { name, type, logo }) => {
-            return await Company.create({ name, type, logo });
-        },
-        //add a new user and link to the company you are in
-        addUser: async (
-            parent,
-            {
-                firstName,
-                lastName,
-                role,
-                title,
-                email,
-                phone,
-                password,
-                profileImage,
-                companyId,
-            }
-        ) => {
-            const user = await User.create({
-                firstName,
-                lastName,
-                role,
-                title,
-                email,
-                phone,
-                password,
-                profileImage,
-                companyId,
-            });
-            const token = signToken(user);
-            return { token, user };
-        },
-        //add a new post
-        addPost: async (parent, { userId, images, text }) => {
-            return await Post.create({ userId, images, text });
-        },
+    company: async (parent, { companyId }) => {
+      return await Company.findOne({ companyId }).populate("users").populate({
+        path: "users",
+        populate: "posts",
+      });
     },
+    //find all users in a company
+    users: async (parent, { companyId }) => {
+      const params = companyId ? { companyId } : {};
+      return await User.find(params).populate("company");
+    },
+    //find all posts in a company
+    posts: async (parent, { companyId }) => {
+      const params = companyId ? { companyId } : {};
+      return await Post.find(params).populate("user").populate({
+        path: "comments",
+        populate: "user",
+      });
+    },
+    //find a single post
+    post: async (parent, { postId }) => {
+      const params = postId ? { postId } : {};
+      return await Post.findOne(params).populate("user").populate({
+        path: "comments",
+        populate: "user",
+      });
+    },
+    //find a selected User
+    user: async (parent, { userId }) => {
+      return await User.findOne({ userId }).populate("company");
+    },
+  },
+  Mutation: {
+    //add a new company
+    addCompany: async (parent, { name, type, logo }) => {
+      return await Company.create({ name, type, logo });
+    },
+    //add a new user and link to the company you are in
+    addUser: async (
+      parent,
+      {
+        firstName,
+        lastName,
+        role,
+        title,
+        email,
+        phone,
+        password,
+        profileImage,
+        companyId,
+      }
+    ) => {
+      const user = await User.create({
+        firstName,
+        lastName,
+        role,
+        title,
+        email,
+        phone,
+        password,
+        profileImage,
+        companyId,
+      });
+
+      // await Company.updateOne({ companyId }, { $push: { users: user } });
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    //add a new post
+    addPost: async (parent, { userId, images, text }) => {
+      return await Post.create({ userId, images, text });
+    },
+  },
 };
 
 module.exports = resolvers;
