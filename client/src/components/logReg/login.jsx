b1d09ada9681de10fdb6d68ba1cfcdf339a379f2
Erik Olson <erikamerico1991@gmail.com>
2023-09-11 01:59:27 -0400

    fairly cool toggling effect  between registration and login forms.

--- client/src/components/logReg/login.jsx
+++ client/src/components/logReg/login.jsx
@@ -1,12 +1,36 @@
 import { Button, Grid, TextField } from "@mui/material";
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./logReg.css";
 
-const Login = () => {
+const Login = ({ toggleForm }) => {
     const [inputs, setInputs] = useState({
         email: "",
         password: "",
     });
+
+    const formContainerRef = useRef(null);
+
+    useEffect(() => {
+        const formContainerHeight = formContainerRef.current.clientHeight;
+        const toggleButton = document.querySelector(".toggleButton");
+            if (toggleButton) {
+        toggleButton.style.height = `${formContainerHeight}px`;
+        }
+    }, []);
+
+    useEffect(() => {
+        document.querySelector(".form-container").classList.toggle("flipLog");
+    }, []);
+
+    useEffect(() => {
+        const toggleButton = document.querySelector(".toggleButton")
+        if (toggleButton) {
+            toggleButton.classList.toggle("slideLog");
+            toggleButton.addEventListener("animationend", () => {
+                toggleButton.style.zIndex = 0;
+            });
+        }
+    }, []);
 
     const handleChange = (e) => {
         setInputs((prevState) => ({
@@ -34,7 +58,8 @@ const Login = () => {
             alignItems="center"
             style={{ minHeight: "100vh" }}
         >
-            <Grid item xs={12} sm={6} className="form-container">
+            <Grid item xs={12} sm={4} className="form-container" ref={formContainerRef}>
+                <h1>Login Form</h1>
                 <form onSubmit={handleSubmit}>
                     <TextField
                         name="email"
@@ -65,11 +90,14 @@ const Login = () => {
                             color="primary"
                             className="submit-button"
                         >
-                            Login
+                            Submit
                         </Button>
                     </Grid>
                 </form>
             </Grid>
+            <div className="toggle-button-container">
+                <Button onClick={toggleForm} variant="contained" className="toggleButton">Register</Button>
+            </div>
         </Grid>
     );
 };
