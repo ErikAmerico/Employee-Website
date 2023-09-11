8c1d736231ae3a0cbd09ad71f7583ca98d944a8b
Michael Tranquillo <michael.r.tranquillo@gmail.com>
2023-09-11 14:49:54 -0400

    basic front end for share component

--- client/src/pages/announcements.jsx
+++ client/src/pages/announcements.jsx
@@ -1,7 +1,12 @@
+import React from "react";
+import Share from "../components/share/Share";
+import Post from "../components/post/Post";
+
 export default function Announcements() {
     return (
         <>
             <h1>Announcements Page</h1>
+            <Share />
         </>
     );
 }
