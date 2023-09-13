31e917db5c14cf9137a31aea0ecf5f94580a53b4
Michael Tranquillo <michael.r.tranquillo@gmail.com>
2023-09-12 21:12:29 -0400

    got post text to save into the database

--- client/src/components/share/Share.jsx
+++ client/src/components/share/Share.jsx
@@ -5,7 +5,39 @@ import Avatar from "@mui/material/Avatar";
 import React from "react";
 import "./share.css";
 
+import { useMutation } from "@apollo/client";
+import { useState } from "react";
+// import Auth from "../../utils/auth";
+import { CREATE_POST } from "../../utils/mutations";
+
 const Share = () => {
+    //set useStates
+    const [postText, setPostText] = useState("");
+
+    //set useMutation
+    const [createPost] = useMutation(CREATE_POST);
+
+    //handle post submit
+    const handlePostSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            const { data } = await createPost({
+                variables: {
+                    postText,
+                },
+            });
+            setPostText("");
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setPostText(value);
+    };
+
     return (
         <div className="share">
             <div className="shareWrapper">
@@ -14,7 +46,9 @@ const Share = () => {
                     <input
                         placeholder="Make and announcement."
                         className="shareInput"
-                    />
+                        name="postText"
+                        onChange={handleChange}
+                    ></input>
                 </div>
                 <hr className="shareHr" />
                 <div className="shareBottom">
@@ -43,7 +77,9 @@ const Share = () => {
                             </span>
                         </div>
                     </div>
-                    <button className="shareButton">Share</button>
+                    <button className="shareButton" onClick={handlePostSubmit}>
+                        Share
+                    </button>
                 </div>
             </div>
         </div>
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,74 +1,102 @@
 import { gql } from "@apollo/client";
 
 export const CREATE_COMPANY = gql`
-  mutation createCompany($name: String!, $type: String!, $logo: String) {
-    createCompany(name: $name, type: $type, logo: $logo) {
-      _id
-      name
-      type
-      logo
+    mutation createCompany($name: String!, $type: String!, $logo: String) {
+        createCompany(name: $name, type: $type, logo: $logo) {
+            _id
+            name
+            type
+            logo
+        }
     }
-  }
 `;
 
 export const CREATE_USER = gql`
-  mutation createUser(
-    $firstName: String!
-    $lastName: String!
-    $email: String!
-    $password: String!
-  ) {
-    createUser(
-      firstName: $firstName
-      lastName: $lastName
-      email: $email
-      password: $password
+    mutation createUser(
+        $firstName: String!
+        $lastName: String!
+        $email: String!
+        $password: String!
     ) {
-      token
-      user {
-        _id
-        email
-      }
+        createUser(
+            firstName: $firstName
+            lastName: $lastName
+            email: $email
+            password: $password
+        ) {
+            token
+            user {
+                _id
+                email
+            }
+        }
     }
-  }
 `;
 
 export const ADD_USER_TO_COMPANY = gql`
-  mutation addUserToCompany($companyId: ID!, $userId: ID!) {
-    addUserToCompany(companyId: $companyId, userId: $userId) {
-      _id
-      name
-      type
-      logo
-      users {
-        _id
-        firstName
-        lastName
-        email
-      }
+    mutation addUserToCompany($companyId: ID!, $userId: ID!) {
+        addUserToCompany(companyId: $companyId, userId: $userId) {
+            _id
+            name
+            type
+            logo
+            users {
+                _id
+                firstName
+                lastName
+                email
+            }
+        }
     }
-  }
 `;
 
 export const LOGIN = gql`
-  mutation Login($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
-      token
-      user {
-        _id
-        name
-        role
-        title
-        company {
-          _id
-          name
-          type
-          logo
+    mutation Login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                name
+                role
+                title
+                company {
+                    _id
+                    name
+                    type
+                    logo
+                }
+                email
+                phone
+                profileImage
+            }
         }
-        email
-        phone
-        profileImage
-      }
     }
-  }
 `;
+
+export const CREATE_POST = gql`
+    mutation createPost($postText: String!, $images: [String]) {
+        createPost(postText: $postText, images: $images) {
+            postText
+        }
+    }
+`;
+
+// export const CREATE_POST = gql`
+//     mutation createPost($postText: String!, $images: [String]) {
+//         createPost(postText: $postText, images: $images) {
+//             _id
+//             postText
+//             user {
+//                 _id
+//                 firstName
+//                 lastName
+//             }
+//             images
+//             likes
+//             comments {
+//                 _id
+//                 text
+//             }
+//         }
+//     }
+// `;
