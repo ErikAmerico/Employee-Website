21fac82818d9a4ca34a8e1d9ae005cadd66c3869
Erik Olson <erikamerico1991@gmail.com>
2023-09-13 21:32:00 -0400

    added note for token, admin, roles

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -35,6 +35,7 @@ module.exports = {
     // return the request object so it can be passed to the resolver as `context`
     return req;
   },
+  //Role should be sent in payload of token, for admin purposes
   signToken: function ({ email, firstName, lastName, _id }) {
     const payload = { email, firstName, lastName, _id };
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
