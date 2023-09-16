fc85b6c6eb7507b95a0036a352c1499083cfbecf
Michael Tranquillo <michael.r.tranquillo@gmail.com>
2023-09-16 19:05:34 -0400

    updated authentication so that it resolves for admins and owners correctly

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,46 +1,58 @@
 import decode from "jwt-decode";
 
 class AuthService {
-  // get user data
-  getProfile() {
-    return decode(this.getToken());
-  }
-  // check if user's logged in
-  loggedIn() {
-    // Checks if there is a saved token and it's still valid
-    const token = this.getToken();
-    return !!token && !this.isTokenExpired(token); // handwaiving here
-  }
-  // check if token is expired
-  isTokenExpired(token) {
-    try {
-      const decoded = decode(token);
-      if (decoded.exp < Date.now() / 1000) {
-        return true;
-      } else return false;
-    } catch (err) {
-      return false;
-    }
-  }
-  getToken() {
-    // Retrieves the user token from localStorage
-    return localStorage.getItem("id_token");
-  }
-  login(idToken) {
-    //saves company id of logged in user to local storage
-    const decodedToken = decode(idToken);
-    localStorage.setItem("company_id", decodedToken.data.company);
+    // get user data
+    getProfile() {
+        const token = this.getToken();
 
-    // Saves user token to localStorage
-    localStorage.setItem("id_token", idToken);
-    window.location.assign("/");
-  }
-  logout() {
-    // Clear user token and profile data from localStorage
-    localStorage.removeItem("id_token");
-    // this will reload the page and reset the state of the application
-    window.location.assign("/");
-  }
+        if (!token) {
+            return null;
+        }
+        try {
+            return decode(token);
+        } catch (err) {
+            console.error(err);
+            return null;
+        }
+    }
+    // check if user's logged in
+    loggedIn() {
+        // Checks if there is a saved token and it's still valid
+        const token = this.getToken();
+        return !!token && !this.isTokenExpired(token); // handwaiving here
+    }
+    // check if token is expired
+    isTokenExpired(token) {
+        try {
+            const decoded = decode(token);
+            if (decoded.exp < Date.now() / 1000) {
+                // remove token from local storage
+                localStorage.removeItem("id_token");
+                return true;
+            } else return false;
+        } catch (err) {
+            return false;
+        }
+    }
+    getToken() {
+        // Retrieves the user token from localStorage
+        return localStorage.getItem("id_token");
+    }
+    login(idToken) {
+        //saves company id of logged in user to local storage
+        const decodedToken = decode(idToken);
+        localStorage.setItem("company_id", decodedToken.data.company);
+
+        // Saves user token to localStorage
+        localStorage.setItem("id_token", idToken);
+        window.location.assign("/");
+    }
+    logout() {
+        // Clear user token and profile data from localStorage
+        localStorage.removeItem("id_token");
+        // this will reload the page and reset the state of the application
+        window.location.assign("/");
+    }
 }
 
 export default new AuthService();
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -5,61 +5,61 @@ const secret = "mysecretssshhhhhhh";
 const expiration = "2h";
 
 module.exports = {
-  AuthenticationError: new GraphQLError("Could not authenticate user.", {
-    extensions: {
-      code: "UNAUTHENTICATED",
+    AuthenticationError: new GraphQLError("Could not authenticate user.", {
+        extensions: {
+            code: "UNAUTHENTICATED",
+        },
+    }),
+    authMiddleware: function ({ req }) {
+        // allows token to be sent via req.body, req.query, or headers
+        let token =
+            req.body.token || req.query.token || req.headers.authorization;
+
+        // We split the token string into an array and return actual token
+        if (req.headers.authorization) {
+            token = token.split(" ").pop().trim();
+        }
+
+        if (!token) {
+            return req;
+        }
+
+        // if token can be verified, add the decoded user's data to the request so it can be accessed in the resolver
+        try {
+            const { data } = jwt.verify(token, secret, { maxAge: expiration });
+            req.user = data;
+        } catch (err) {
+            console.log("Invalid token");
+            console.error(err);
+            //if there is an error, the req gets returned regardless, not ideal?
+            // throw new AuthenticationError("Invalid token");
+        }
+
+        // return the request object so it can be passed to the resolver as `context`
+        return req;
     },
-  }),
-  authMiddleware: function ({ req }) {
-    // allows token to be sent via req.body, req.query, or headers
-    let token = req.body.token || req.query.token || req.headers.authorization;
 
-    // We split the token string into an array and return actual token
-    if (req.headers.authorization) {
-      token = token.split(" ").pop().trim();
-    }
-
-    if (!token) {
-      return req;
-    }
-
-    // if token can be verified, add the decoded user's data to the request so it can be accessed in the resolver
-    try {
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
-    } catch (err) {
-      console.log("Invalid token");
-      console.error(err);
-      //if there is an error, the req gets returned regardless, not ideal?
-      // throw new AuthenticationError("Invalid token");
-    }
-
-    // return the request object so it can be passed to the resolver as `context`
-    return req;
-  },
-  //checkIfadminAUth goes here maybe
-
-  //Role should be sent in payload of token, for admin purposes
-  signToken: function ({
-    email,
-    firstName,
-    lastName,
-    _id,
-    role,
-    company,
-    title,
-    phone,
-  }) {
-    const payload = {
-      email,
-      firstName,
-      lastName,
-      _id,
-      role,
-      company,
-      title,
-      phone,
-    };
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  },
+    //Role should be sent in payload of token, for admin purposes
+    signToken: function ({
+        email,
+        firstName,
+        lastName,
+        _id,
+        role,
+        company,
+        title,
+        phone,
+    }) {
+        const payload = {
+            email,
+            firstName,
+            lastName,
+            _id,
+            role,
+            company,
+            title,
+            phone,
+        };
+        return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+    },
 };
