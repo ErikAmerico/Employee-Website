e90f4a7324c7ea5d245d8be75f9857524a326753
Erik Olson <erikamerico1991@gmail.com>
2023-09-30 13:00:30 -0400

    removed window, using useLocation again. added serverPort variable to server.js

--- server/server.js
+++ server/server.js
@@ -16,64 +16,65 @@ const app = express();
 app.use(cors());
 const socketServer = http.createServer(app);
 const io = new Server(socketServer, {
-    cors: {
-        // origin: ["http://localhost:3000", "http://127.0.0.1:3000", "https://componnect-3a658d1d17cf.herokuapp.com"]
-        origin: "https://componnect-3a658d1d17cf.herokuapp.com"
-    },
+  cors: {
+    // origin: ["http://localhost:3000", "http://127.0.0.1:3000", "https://componnect-3a658d1d17cf.herokuapp.com"]
+    origin: "https://componnect-3a658d1d17cf.herokuapp.com",
+  },
 });
 
 io.on("connection", (socket) => {
-    console.log(`A user connected: ${socket.id}`);
+  console.log(`A user connected: ${socket.id}`);
 
-    socket.on("send_message", (data) => {
-        console.log(data);
-        socket.broadcast.emit("receive_message", data);
-        socket.emit("receive_message", data);
-    });
+  socket.on("send_message", (data) => {
+    console.log(data);
+    socket.broadcast.emit("receive_message", data);
+    socket.emit("receive_message", data);
+  });
 
-    socket.on("disconnect", () => {
-        console.log("A user disconnected");
-    });
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
 });
 
-socketServer.listen(3002, () => {
-    console.log("Socket server running on port 3002");
+const serverPort = process.env.PORT || 3002;
+socketServer.listen(serverPort, () => {
+  console.log(`Socket server running on port ${serverPort}`);
 });
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    //context: ({ req }) => ({ req, socket }), //passing socket via context to resolvers
+  typeDefs,
+  resolvers,
+  //context: ({ req }) => ({ req, socket }), //passing socket via context to resolvers
 });
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async () => {
-    await server.start();
+  await server.start();
 
-    app.use(express.urlencoded({ extended: false }));
-    app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
 
-    app.use(
-        "/graphql",
-        expressMiddleware(server, {
-            context: authMiddleware,
-        })
-    );
+  app.use(
+    "/graphql",
+    expressMiddleware(server, {
+      context: authMiddleware,
+    })
+  );
 
-    if (process.env.NODE_ENV === "production") {
-        app.use(express.static(path.join(__dirname, "../client/dist")));
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/dist")));
 
-        app.get("*", (req, res) => {
-            res.sendFile(path.join(__dirname, "../client/dist/index.html"));
-        });
-    }
-
-    db.once("open", () => {
-        app.listen(PORT, () => {
-            console.log(`API server running on port ${PORT}!`);
-            console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
-        });
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../client/dist/index.html"));
     });
+  }
+
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+    });
+  });
 };
 
 // Call the async function to start the server
